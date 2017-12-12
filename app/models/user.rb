0252b5a9a65f2665484d4e30f838de64f2b4fe5e
Lisa Lee <snickers495@gmail.com>
2017-12-11 22:08:24 -0600

    finished all models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :carts
   has_many :items, through: :carts
-  belongs_to :current_cart, class_name: "Cart", foreign_key: "cart_id"
-  has_secure_password
+  belongs_to :current_cart, class_name: "Cart", foreign_key: "current_cart"
+  has_secure_password validations: false
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212003050) do
+ActiveRecord::Schema.define(version: 20171212035947) do
 
   create_table "carts", force: :cascade do |t|
     t.string  "name"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 20171212003050) do
 
   create_table "users", force: :cascade do |t|
     t.string  "username"
-    t.string  "password"
     t.integer "current_cart"
     t.string  "password_digest"
     t.string  "email"
